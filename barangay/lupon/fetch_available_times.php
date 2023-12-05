bb8ec53007247e7f933d126f57a732930c369743
Jamie <memongspov@gmail.com>
2023-12-05 20:20:28 +0800

    query is now grouped per barangay

--- barangay/lupon/fetch_available_times.php
+++ barangay/lupon/fetch_available_times.php
@@ -2,15 +2,24 @@
 
 include '../../config.php';
 
-// Assuming you have a table named 'availability' with columns 'date' and 'time'
-// Adjust the table and column names according to your database structure
+session_start();
+
+$email = $_SESSION['email_address'];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['selectedDate'])) {
     $selectedDate = $_POST['selectedDate'];
 
-    // Fetch existing times from the database based on the selected date
-    $query = "SELECT DISTINCT TIME_FORMAT(time_of_hearing, '%H:%i:%s') AS formatted_time FROM hearing WHERE date_of_hearing = '$selectedDate'";
-    $result = mysqli_query($conn, $query);
+     $selectLuponId = mysqli_query($conn, "SELECT pb_id FROM `lupon_accounts` WHERE email_address = '$email'");
+    
+     if (!$selectLuponId) {
+         die('Failed to fetch pb_id: ' . mysqli_error($conn));
+     }
+ 
+     $row = mysqli_fetch_assoc($selectLuponId);
+     $pb_id = $row['pb_id'];
+
+     $query = "SELECT DISTINCT TIME_FORMAT(time_of_hearing, '%H:%i:%s') AS formatted_time FROM hearing WHERE date_of_hearing = '$selectedDate' AND pb_id = '$pb_id'";
+     $result = mysqli_query($conn, $query);
 
     if ($result) {
         $existingTimes = array();
@@ -19,16 +28,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['selectedDate'])) {
             $existingTimes[] = $row['formatted_time'];
         }
 
-        // Default timeslots
         $defaultTimeslots = array('8:00:00', '9:00:00', '10:00:00', '11:00:00', '13:00:00', '14:00:00', '15:00:00', '16:00:00', '17:00:00');
 
-        // Exclude existing times from default timeslots
         $availableTimes = array_diff($defaultTimeslots, $existingTimes);
 
-        // Return the available times as a JSON array
         echo json_encode(array_values($availableTimes));
     } else {
-        // Handle the query error
         echo json_encode(array());
     }
 }
